807719ee9e48ba508601a7ec91d152c0f0b2ff9b
vasek <vasia.motnin@gmail.com>
2022-03-16 17:08:45 +0200

    update

--- src/convertStyleNameInClassName/tagConverters/convertOnlyStyleName.js
+++ src/convertStyleNameInClassName/tagConverters/convertOnlyStyleName.js
@@ -2,7 +2,24 @@ const config = require('../config')
 
 module.exports = function convertOnlyStyleName({tag, index, size, content}) {
 
-  console.log(content, tag)
+  const newTagWithAttributeName = convertAttributeName(tag)
+  const newTagWithAttributeValue = convertAttributeValue(newTagWithAttributeName)
+
+  // console.log(newTag)
+
+  // console.log(content, tag)
 
   return content
+}
+
+
+function convertAttributeName(tag) {
+  return tag.replace(/styleName/, 'className')
+}
+
+function convertAttributeValue(tag) {
+
+  console.log(tag)
+
+  return tag
 }
