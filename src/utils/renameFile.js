b4dd55ae8b6218914658c37f8b8f705b4e2bf034
vasek <vasia.motnin@gmail.com>
2022-04-08 11:08:17 +0300

    add convertTsToTsx

--- src/utils/renameFile.js
+++ src/utils/renameFile.js
@@ -1,3 +1,12 @@
-module.exports = function renameFile({prevPath, newPath}) {
-  console.log(prevPath, newPath)
+const fs = require('fs')
+
+module.exports = async function renameFile({prevPath, newPath}) {
+
+  try {
+    fs.renameSync(prevPath, newPath)
+
+    console.log(`--> ${newPath} ✓`)
+  } catch (error) {
+    console.log(error)
+  }
 }
